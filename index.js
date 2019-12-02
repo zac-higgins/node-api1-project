bae039805da9b78d6fb4012931119bed31f28f75
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-02 15:23:22 -0700

    added conditional error message for instances where a user id is requested that doesn't exist

--- index.js
+++ index.js
@@ -47,10 +47,15 @@ server.get('/api/users/:id', (req, res) => {
 
     db.findById(id)
         .then(user => {
-            res.status(200).json(user);
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            }
+
         })
         .catch(err => {
             console.log(`error on GET /api/users/${id}`, err);
-            res.status(500).json({ message: "The user with the specified ID does not exist." })
+            res.status(500).json({ error: "The user information could not be retrieved." })
         });
 })
