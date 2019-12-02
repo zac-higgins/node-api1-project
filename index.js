ad0f9a7504739a3f60df837cadc896967ff22d2f
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-02 15:05:23 -0700

    added POST endpoint

--- index.js
+++ index.js
@@ -5,10 +5,29 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.send({ api: 'up and running...' })
+    res.send('It\'s working!!')
 });
 
 const port = 5000;
 server.listen(port, () => {
     console.log(`\n ** API running on port ${port} ** \n`)
 })
+
+server.post('/api/users', (req, res) => {
+    const userData = req.body;
+
+    if (!userData.name || !userData.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+        db.insert(userData)
+            .then(user => {
+                res.status(201).json({ message: "user added successfully" })
+            })
+            .catch(err => {
+                console.log('error on POST /api/users', err);
+                res.status(500).json({ error: "There was an error while saving the user to the database" })
+            })
+
+    }
+})
+
