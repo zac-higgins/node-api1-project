a9fee205b2376cd3fcf2cb60331841f428281b76
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-02 13:56:02 -0700

    installed dependencies. setup API boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send({ api: 'up and running...' })
+});
+
+const port = 5000;
+server.listen(port, () => {
+    console.log(`\n ** API running on port ${port} ** \n`)
+})
