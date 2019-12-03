aa3adb7103a4c73bc405e8edb9b6d56b1e73a97f
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-02 17:41:36 -0700

    added endpoints for delete and put

--- index.js
+++ index.js
@@ -59,3 +59,50 @@ server.get('/api/users/:id', (req, res) => {
             res.status(500).json({ error: "The user information could not be retrieved." })
         });
 })
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+        .then(user => {
+            if (user) {
+                res.status(200).json({ message: 'user removed successfully' })
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            console.log('error on DELETE /api/users/:id', err);
+            res.status(500).json({ error: "The user could not be removed" })
+        });
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const userData = req.body;
+
+    if (!userData.name || !userData.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+        db.update(id, userData)
+            .then(user => {
+                if (user) {
+                    db.findById(id)
+                        .then(user => {
+                            res.status(200).json(user);
+                        })
+                        .catch(err => {
+                            console.log(`error on GET /api/users/${id}`, err);
+                            res.status(500).json({ error: "The user information could not be retrieved." })
+                        })
+                } else {
+                    res.status(404).json({ message: "The user with the specified ID does not exist." })
+                }
+            })
+            .catch(err => {
+                console.log('error on PUT /api/users/:id', err);
+                res.status(500).json({ error: "The user information could not be modified." })
+            });
+    }
+
+})
